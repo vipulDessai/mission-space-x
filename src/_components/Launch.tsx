fe0ef98c0f6cbc77278185006e69afefd0fa7706
Vipul Dessai <badgamerbad@gmail.com>
2021-01-17 18:08:23 +0530

    mids in list

--- src/_components/Launch.tsx
+++ src/_components/Launch.tsx
@@ -8,22 +8,27 @@ export function Launch({launchData}: {launchData: LaunchInterface}) {
     const title = `${launchData.mission_name} # ${launchData.flight_number}`;
     const rocketImg = !launchData.links.mission_patch_small ? <img src={defaultImage} alt={launchData.rocket.rocket_name} /> : <img src={launchData.links.mission_patch_small} alt={launchData.rocket.rocket_name} />;
     const rocketData: any = {
-        "Mission Ids": launchData.mission_id.length > 0 ? launchData.mission_id.join() : 'Not Available',
         "Launch Year": launchData.launch_year,
         "Successful Launch": JSON.stringify(launchData.launch_success),
     }
 
     const cores = launchData.rocket.first_stage.cores;
     if(cores && cores[0] && cores[0].land_success !== null && cores[0].land_success !== undefined)
-        rocketData["Successful Landing"] = JSON.stringify(launchData.rocket.first_stage.cores[0].land_success);
+        rocketData["Successful Landing"] = JSON.stringify(cores[0].land_success);
     
     return (
         <>
             <li key={0}>{rocketImg}</li>
             <li key={1}><ul><li><b>{title}</b></li></ul></li>
+            <li key={2}>
+                <ul>
+                    <li><b>Mission Ids: </b></li>
+                    {launchData.mission_id.length > 0 ? launchData.mission_id.map(id => <li className="p-l-sm">&bull;&nbsp;{id}</li>) : <li>Not Available</li>}
+                </ul>
+            </li>
             {
                 Object.keys(rocketData).map((key, index) => (
-                    <li key={index + 2}>
+                    <li key={index + 3}>
                         <ul className="details">
                             <li key={0}><b>{key}:&nbsp;</b></li>
                             <li key={1}>{rocketData[key]}</li>
