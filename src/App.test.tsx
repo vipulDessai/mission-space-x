91eddefd00ca383f20f5e893532fd4dbe6c05b53
Vipul Dessai <badgamerbad@gmail.com>
2021-01-17 22:45:30 +0530

    code cleanup - tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history'
 
 import { App } from '@/App';
